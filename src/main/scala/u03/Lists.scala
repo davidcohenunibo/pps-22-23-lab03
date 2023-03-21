e652a398b437e394f3edc8b76c2c2182ec53950a
David <david.cohen@studio.unibo.it>
2023-03-21 15:29:35 +0100

    refactor

--- src/main/scala/u03/Lists.scala
+++ src/main/scala/u03/Lists.scala
@@ -44,7 +44,7 @@ object Lists extends App :
 
     def append[A](l: List[A], r: List[A]): List[A] = (l, r) match
       case (Cons(h, t), r) => Cons(h, append(t, r))
-      case (Nil(), r) => r
+      case (_, r) => r
 
     def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match
       case Cons(h, t) => append(f(h), flatMap(t)(f))
@@ -52,32 +52,32 @@ object Lists extends App :
 
     def map[A, B](l: List[A])(mapper: A => B): List[B] = l match
       case Cons(_,_) => flatMap(l)(a => Cons(mapper(a), Nil()))
-      case Nil() => Nil()
+      case _ => Nil()
 
     def filter[A](l: List[A])(pred: A => Boolean): List[A] = l match
       case Cons(_,_) => flatMap(l)(a => if pred(a) then Cons(a, Nil()) else Nil())
-      case Nil() => Nil()
+      case _ => Nil()
 
     @tailrec
     def drop[A](l: List[A], i: Int): List[A] = (l,i) match
       case (Cons(_, t),1) => t
       case (Cons(_, t),i) => drop(t,i-1)
-      case (Nil(),_) => Nil()
+      case _ => Nil()
 
     def max(l: List[Int]): Option[Int] = l match
       case Cons(h,t) => Some(max(t) match
         case Some(m) => if h > m then h else m
-        case None() => h)
-      case Nil() => None()
+        case _ => h)
+      case _ => None()
 
-    def getCoursesByList(l: List[Person]): List[String] = flatMap(l)(p => p match
-      case Student(_,_) => Nil()
-      case Teacher(_,c) => Cons(c, Nil()))
+    def getCoursesByList(l: List[Person]): List[String] = flatMap(l)(_ match
+      case Teacher(_,c) => Cons(c, Nil())
+      case _ => Nil())
 
     @tailrec
     def foldLeft[A,B](l: List[A])(acc: B)(op: (B,A) => B): B = l match
       case Cons(h,t) => foldLeft(t)(op(acc,h))(op)
-      case Nil() => acc
+      case _ => acc
     def foldRight[A,B](l: List[A])(acc: B)(op: (A,B) => B): B = l match
       case Cons(h,t) => op(h, foldRight(t)(acc)(op))
-      case Nil() => acc
+      case _ => acc
